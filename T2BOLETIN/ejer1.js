59d4fde3f056f5dda895a69a49bce784ef34dfa8
manuelwito <manuel.ramos.illana.al@iespoligonosur.org>
2022-03-17 12:27:07 +0100

    dia 1

---
+++ T2BOLETIN/ejer1.js
@@ -0,0 +1,7 @@
+//Calcular el área y el volumen de la esfera cuyo radio se pide al usuario.
+let radio = prompt("Indique el radio de la esfera");
+//a = 4 π r 2 .
+var area = 4 * 3.14 * (radio * radio);
+alert("El area de la esfera es: " + area);
+
+
---
+++ T2BOLETIN/ejer2.js
@@ -0,0 +1,5 @@
+//Diseñar un algoritmo para hallar el valor absoluto de un número leído de teclado y presentarlo en pantalla.
+
+var numero = prompt("indroduzca un número para calcular su valor absoluto: ");
+var resultado = Math.abs(numero);
+alert("El valor absoluto de " + numero + " es " + resultado);
