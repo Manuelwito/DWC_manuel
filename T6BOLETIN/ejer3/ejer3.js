e8ec6fc35866ce335ee3d2fd9787483cb05a49eb
manuelwito <manuel.ramos.illana.al@iespoligonosur.org>
2022-06-07 11:04:16 +0200

    ejer3

---
+++ T6BOLETIN/ejer3/ejer3.js
@@ -0,0 +1,6 @@
+// Crea un documento HTML con una lista de modelos de coches 
+
+//cuyo estilo sea de cuadradillo. Escribe una función JavaScript que use la f. getElementsByTagName() para recuperar los elementos de la lista. Añade el estilo de cuadradilla de la lista mediante JavaScript. Añade un nuevo modelo a la lista usando DOM, al principio, al final y en alguna posición intermedia que elijas. 
+	//Imagina que tenemos que insertar un nuevo modelo de coche, por ejemplo “BMW”, pero 	no conocemos en qué posición de la lista de nodos hijos está. Busca ese elemento por 		su texto y añade el nuevo modelo antes y después de este.
+
+
